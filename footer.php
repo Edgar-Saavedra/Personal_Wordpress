22447ff402b5765e78cf313d8bf3391c4983b343
Edgar-Saavedra <edgarsaavedraa@gmail.com>
2012-11-02 17:10:52 -0700

    First Commit For Personal WordPress theme

---
+++ footer.php
@@ -0,0 +1,41 @@
+
+	
+<!--start footer-->
+			<div id="footercontainer">
+				<div id="footerbio"><img src= "./img/profilephoto.png" /> 
+					<div id="footerbiotext">Edgar Saavedra<br/>
+					<span id="bioparagraph">
+					Lorem ipsum dolor sit amet, consectetur 
+					adipiscing elit. Phasellus adipiscing sodales lectus vitae dictum. 
+					Aliquam ultrices nisi eu mi fermentum in tempus quam pellentesque.
+					Cras non mattis dolor. Donec lacus sem Lorem ipsum dolor sit amet, consectetur 
+					adipiscing elit. Phasellus adipiscing sodales lectus vitae dictum. 
+					Aliquam ultrices nisi eu mi fermentum in tempus quam pellentesque.
+					Cras non mattis dolor. Donec lacus sem,
+				 </span></div> 
+				 <div id="footercontact">
+				<span style="font-weight:bold;">Contact</span> <br/>
+				PH: 773.627.8706</br>
+				Email:<a href="mailto:edgarsaavedraa@gmail.com" style="a:white"> edgarsaavedraa@gmail.com</a><br/>
+				<a href="" style="color:white; font-weight:bold; text-decoration:underline;" >Resume</a>
+					</div>
+					</div>
+				
+				<div id="footerwhite">
+					<img src="./img/logo-01-01.png" border="0"/>
+					<span id="copyright">    copyright &copy; Edgar Saaavedra 2012</span>
+				</div>
+				
+			</div>
+<!--end footer-->
+	
+
+		</div>
+<!--end container-->	
+	<?php wp_footer(); ?>
+	
+	<!-- Don't forget analytics -->
+	
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,75 @@
+<?php get_header(); ?>
+
+
+	<!--start printmaking section-->
+			<div id="print">
+				
+					<div class="gallery">
+	                    <a class="thumb">
+						<img src='/img/gal1thumb.png" width="75px" height="600px' border="0"/>
+						<span><img src="/img/print1.png"/><br/>text</span></a>
+						
+						<a class="thumb">
+						<img src="./img/gal2thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						<a class="thumb">
+						<img src="./img/gal3thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						<a class="thumb">
+						<img src="./img/gal4thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						<a class="thumb">
+						<img src="./img/gal5thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+				    </div>
+					<div id="ptext">Printmaking </div>
+					<div class="pfont">Lorem ipsum dolor sit amet, consectetur 
+					adipiscing elit. Phasellus adipiscing sodales lectus vitae dictum. 
+					Aliquam ultrices nisi eu mi fermentum in tempus quam pellentesque.
+					Cras non mattis dolor. Donec lacus sem, .</div> 
+			</div>
+<!--end printmaking section-->
+<!--start design section-->
+			<div id="design">
+				 
+					<div class="dgallery">
+	                    <a class="dthumb">
+						<img src="./img/gal8thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						 <a class="dthumb">
+						<img src="./img/gal6thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						 <a class="dthumb">
+						<img src="./img/gal9thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						 <a class="dthumb">
+						<img src="./img/gal7thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+						
+						 <a class="dthumb">
+						<img src="./img/gal10thumb.png" width="75px" height="600px" border="0"/>
+						<span><img src="./img/print1.png"/><br/>text</span></a>
+					
+				    </div>
+					
+					
+					
+					<div class="dfont"><div class="dtext">Design </div>Lorem ipsum dolor sit amet, consectetur 
+					adipiscing elit. Phasellus adipiscing sodales lectus vitae dictum. 
+					Aliquam ultrices nisi eu mi fermentum in , .</div>
+					
+					
+					
+			</div>
+
+<!--end design-->
+
+
+<?php get_footer(); ?>
